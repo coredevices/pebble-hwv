828baaa815b92b9431cef35b187ba179743b5c3d
Gerard Marull-Paretas <gerard@teslabs.com>
2025-04-28 23:10:00 +0200

    app: flash: add stress test
    Based on
    https://github.com/pebble-dev/pebble-firmware/commit/ecd5f569024444a126b17fa6773228c427be2c34
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- app/src/flash.c
+++ app/src/flash.c
@@ -4,10 +4,21 @@
 #include <zephyr/drivers/regulator.h>
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
+#include <zephyr/random/random.h>
 #include <zephyr/shell/shell.h>
+
+#define FLASH_STRESS_TEST_SIZE 1024
 
 static const struct device *const flash = DEVICE_DT_GET(DT_ALIAS(flash0));
 static bool initialized;
+
+static uint32_t xorshift32(uint32_t seed)
+{
+	seed ^= seed << 13;
+	seed ^= seed >> 17;
+	seed ^= seed < 5;
+	return seed;
+}
 
 static int cmd_flash_id(const struct shell *sh, size_t argc, char **argv)
 {
@@ -184,11 +195,117 @@ static int cmd_flash_write(const struct shell *sh, size_t argc, char **argv)
 	return ret;
 }
 
+static int cmd_flash_stress(const struct shell *sh, size_t argc, char **argv)
+{
+	int ret;
+	unsigned int iters;
+	struct flash_pages_info info;
+	uint8_t *buf;
+	uint64_t flash_size;
+
+	if (!initialized) {
+		shell_error(sh, "Flash module not initialized");
+		return -EPERM;
+	}
+
+	if (argc < 2) {
+		iters = 1;
+	} else {
+		iters = (unsigned int)strtoul(argv[1], NULL, 0);
+	}
+
+	ret = flash_get_size(flash, &flash_size);
+	if (ret < 0) {
+		shell_error(sh, "Failed to get flash size (%d)", ret);
+		return ret;
+	}
+
+	buf = k_malloc(FLASH_STRESS_TEST_SIZE);
+	if (buf == NULL) {
+		shell_error(sh, "Buffer allocation failed");
+		return -ENOMEM;
+	}
+
+	ret = pm_device_action_run(flash, PM_DEVICE_ACTION_RESUME);
+	if (ret < 0) {
+		shell_error(sh, "Failed to resume flash (%d)", ret);
+		goto err_free;
+	}
+
+	for (unsigned int i = 0; i < iters; i++) {
+		uint32_t addr, lfsr_seed, lfsr_cur;
+
+		addr = sys_rand32_get() % flash_size;
+
+		ret = flash_get_page_info_by_offs(flash, addr, &info);
+		if (ret < 0) {
+			shell_error(sh, "Failed to get page info (%d)", ret);
+			goto err_suspend;
+		}
+
+		shell_print(sh, "Stress test iteration %u: addr 0x%08x", i, (uint32_t)info.start_offset);
+
+		ret = flash_erase(flash, info.start_offset, info.size);
+		if (ret < 0) {
+			shell_error(sh, "Failed to erase flash (%d)", ret);
+			goto err_suspend;
+		}
+
+		lfsr_seed = sys_rand32_get();
+		if (lfsr_seed == 0U) {
+			lfsr_seed = 1U;
+		}
+
+		lfsr_cur = lfsr_seed;
+		for (unsigned int i = 0U; i < FLASH_STRESS_TEST_SIZE; i++) {
+			buf[i] = (uint8_t)lfsr_cur;
+			lfsr_cur = xorshift32(lfsr_cur);
+		}
+
+		ret = flash_write(flash, info.start_offset, buf, FLASH_STRESS_TEST_SIZE);
+		if (ret < 0) {
+			shell_error(sh, "Failed to write to flash (%d)", ret);
+			goto err_suspend;
+		}
+
+		memset(buf, 0, FLASH_STRESS_TEST_SIZE);
+		ret = flash_read(flash, info.start_offset, buf, FLASH_STRESS_TEST_SIZE);
+		if (ret < 0) {
+			shell_error(sh, "Failed to read from flash (%d)", ret);
+			goto err_suspend;
+		}
+
+		lfsr_cur = lfsr_seed;
+		for (unsigned int i = 0U; i < FLASH_STRESS_TEST_SIZE; i++) {
+			if (buf[i] != (uint8_t)lfsr_cur) {
+				shell_error(
+					sh,
+					"Miscompare at offset %u: expected 0x%02x, found 0x%02x", 0,
+					(uint8_t)lfsr_cur, buf[i]);
+				ret = -EINVAL;
+				goto err_suspend;
+			}
+			lfsr_cur = xorshift32(lfsr_cur);
+		}
+	}
+
+	shell_print(sh, "Flash stress test done! All %u iterations passed", iters);
+
+err_suspend:
+	(void)pm_device_action_run(flash, PM_DEVICE_ACTION_SUSPEND);
+
+err_free:
+	k_free(buf);
+
+	return ret;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_flash_cmds, SHELL_CMD(id, NULL, "Read flash ID", cmd_flash_id),
 	SHELL_CMD_ARG(erase, NULL, "Erase page: erase PAGE_ADDR", cmd_flash_erase, 2, 0),
 	SHELL_CMD_ARG(read, NULL, "Read: read ADDR NUM_BYTES", cmd_flash_read, 3, 0),
 	SHELL_CMD_ARG(write, NULL, "Write: write ADDR DATA", cmd_flash_write, 3, 0),
+	SHELL_CMD_ARG(stress, NULL, "Stress: stress [ITERS]", cmd_flash_stress, 1, 1),
 	SHELL_SUBCMD_SET_END);
 
 SHELL_SUBCMD_ADD((hwv), flash, &sub_flash_cmds, "Flash", NULL, 0, 0);
